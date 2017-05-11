a4a9d714a8a070802077ab1cfc1553873d65e3d4
ymofen@diocp.org <ymofen@diocp.org>
2017-05-11 10:08:12 +0800

    * faq.txt

--- samples/DIOCPHttpServer/diocpHttpSERVER.dpr
+++ samples/DIOCPHttpServer/diocpHttpSERVER.dpr
@@ -5,9 +5,6 @@ uses
   ufrmMain in 'ufrmMain.pas' {frmMain},
   uFMMonitor in '..\Common\Frames\uFMMonitor.pas' {FMMonitor: TFrame},
   uRunTimeINfoTools in '..\Common\Frames\uRunTimeINfoTools.pas',
-  superobject in 'superobject.pas',
-  diocp.ex.SimpleMsgPackSession in 'diocp.ex.SimpleMsgPackSession.pas',
-  SimpleMsgPack in 'SimpleMsgPack.pas',
   utils_websocket in '..\..\source\utils_websocket.pas';
 
 {$R *.res}
